7d1f5e5d7487c3b3205390ad010707730001a849
beringuy <beringuyad@gmail.com>
2023-07-17 11:07:39 -0300

    Atualizado em 17jul2023

--- src/Page_2.svelte
+++ src/Page_2.svelte
@@ -19,31 +19,40 @@
 <p1>
 	<img style="vertical-align:middle" height=25 src="./img/item_1.png"> Ao fim da consulta, certifique-se que o ambiente está silencioso. Dê as devidas instruções ao paciente sobre o procedimento que irá realizar.
 	<br>
+	<br>
 	<img  height=200 src="./img/fig_1.png">
 	<spacing>-</spacing>
 	<br>
+	<br>
 	<img style="vertical-align:middle" height=25 src="./img/item_2.png"> Nas etapas de gravação, mantenha o aparelho à uma distância de 30-60cm do paciente. Não obstrua ou cubra o aparelho.
+	<br>
 	<br>	
 	<img  height=200 src="./img/fig_2.png">
 	<spacing>-</spacing>
 	<br>
+	<br>
 	<img style="vertical-align:middle" height=25 src="./img/item_3.png"> Solicite ao paciente tossir assim que o botão ‘Gravar’ for pressionado. A gravação será interrompida quando o botão ‘Parar’ for pressionado ou após 5 segundos. Repita este mesmo processo para a vogal sustentada e para a frase.
 	<br>
-	<img  height=200 src="./img/fig_23.png">
-	<spacing>-</spacing>
-	<br>
-	<img style="vertical-align:middle" height=25 src="./img/item_4.png"> Preencha o formulário de anamnese com atenção.
 	<br>
 	<img  height=200 src="./img/fig_3.png">
 	<spacing>-</spacing>
 	<br>
-	<img style="vertical-align:middle" height=25 src="./img/item_5.png"> Confirme e estará pronto!!
+	<br>
+	<img style="vertical-align:middle" height=25 src="./img/item_4.png"> Preencha o formulário de anamnese com atenção.
+	<br>
 	<br>
 	<img  height=200 src="./img/fig_4.png">
+	<spacing>-</spacing>
 	<br>
-	<img style="vertical-align:middle" height=25 src="./img/item_6.png"> Não deixe de higienizar seu aparelho, evitando o risco de contaminação e transmissão de doenças.
+	<br>
+	<img style="vertical-align:middle" height=25 src="./img/item_5.png"> Confirme e estará pronto!!
+	<br>
 	<br>
 	<img  height=200 src="./img/fig_5.png">
+	<br>
+	<br>
+	<img style="vertical-align:middle" height=25 src="./img/item_6.png"> Não deixe de higienizar seu aparelho, evitando o risco de contaminação e transmissão de doenças.
+	
 </p1>
 <br>
 
